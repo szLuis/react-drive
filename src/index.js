5364bd963b36266f7e563f66afb35219b2fac027
Luis Salazar <szluis@outlook.com>
2018-07-23 20:40:53 -0400

    service worker commented, react-templates-loader added in prod

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import DriveDashboard from './DriveDashboard';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<DriveDashboard />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
